fb57a929e675598b3d7f58a91fb854f1256eece4
Reid Rumack <rumackr@msoe.edu>
2018-03-29 16:55:55 -0500

    Finished

--- gcontext.cpp
+++ gcontext.cpp
@@ -19,11 +19,10 @@ GraphicsContext::~GraphicsContext()
 
 
 /**
- * This is a naive implementation that uses floating-point math
- * and "setPixel" which will need to be provided by the concrete
- * implementation.
- * 
- * Parameters:
+ * Draws a line from point (x0,y0) to (x1,y1) using the Bresenham's
+ * line algorithm. Only use integer arithmetic.
+ * @brief  Draws a line from point (x0,y0) to (x1,y1).
+ *
  * @param x0  origin of line
  * @param y0  origin of line
  * @param x1 end of line
@@ -40,79 +39,140 @@ void GraphicsContext::drawLine(int x0, int y0, int x1, int y1)
 	if (dx != 0 || dy !=0)
 	{
 		// slope < 1?
-		if (std::abs(dx)>std::abs(dy))
-		{	// iterate over x
-			double slope = (double)dy/dx;
-			
-			// x increment - need to know which way to go
-			int incx = std::abs(dx)/dx;  // will be 1 or -1
-			
-			for (int x = x0; x != x1; x += incx)
-			{
-				setPixel(x,y0+slope*(x-x0));
-			}
-			
-			// loop ends on iteration early - catch endpoint
-			setPixel(x1,y1);
-		} // end of if |slope| < 1 
-		else 
-		{	// iterate over y
-			double slope = (double)dx/dy;
-			
-			// x increment - need to know which way to go
-			int incy = std::abs(dy)/dy;  // will be 1 or -1
-			
-			for (int y = y0; y != y1; y += incy)
-			{
-				setPixel(x0+slope*(y-y0),y);
-			}
-			
-			// loop ends on iteration early - catch endpoint
-			setPixel(x1,y1);
+		if (std::abs(dx)>std::abs(dy)) {
+			// iterate over x
+			//is the reversed
+			(dx < 0) ? (drawLineLow(x1, y1, x0, y0)) : (drawLineLow(x0, y0, x1, y1));
+			// end of if |slope| < 1
+		} else {
+			// iterate over y
+			// is the slope negative
+			(dy < 0) ? (drawLineHigh(x1,y1,x0,y0)) : (drawLineHigh(x0,y0,x1,y1));
+
 		} // end of else |slope| >= 1
 	} // end of if it is a real line (dx!=0 || dy !=0)
 	return;
 }
+/**
+ * Draws a line with slope less then 1
+ * (helper function for drawLine)
+ * @param x0  origin of line
+ * @param y0  origin of line
+ * @param x1 end of line
+ * @param y1 end of line
+ */
+void GraphicsContext::drawLineLow(const int x0, const int y0, const int x1, const int y1) {
+	int dx,dy;
+	int yIntercept = 1;
 
+	//find slope
+	dx = x1 - x0;
+	dy = y1 - y0;
+	if(dy < 0){	//negative slope
+		yIntercept = -1;
+		dy = -dy;
+	}
+
+	int D = (dy << 1) - dx;
+	int y = y0;
+	for (int x = x0; x < x1; ++x) {
+		setPixel(x,y);
+		if (D > 0){
+			y+= yIntercept;
+			D-= (dx << 1);
+		}
+		D += (dy << 1);
+	}
+
+}
+/**
+ * Draws a line with slope grater then 1
+ * (helper function for drawLine)
+ * @param x0  origin of line
+ * @param y0  origin of line
+ * @param x1 end of line
+ * @param y1 end of line
+ */
+void GraphicsContext::drawLineHigh(const int x0, const int y0, const int x1, const int y1) {
+	int dx,dy;
+	int xIntercept = 1;
+
+	//find slope
+	dx = x1 - x0;
+	dy = y1 - y0;
+
+	if(dx < 0){ // reversed line
+		xIntercept = -1;
+		dx = -dx;
+	}
+
+	int D = (dx << 1) - dy;
+	int x = x0;
+	for (int y = y0; y < y1; ++y) {
+		setPixel(x,y);
+		if (D > 0){
+			x+= xIntercept;
+			D-= (dy << 1);
+		}
+		D += (dx << 1);
+	}
+
+}
 
 
 /**
- * This is a naive implementation that uses floating-point math
- * and "setPixel" which will need to be provided by the concrete
- * implementation.
- * 
- * Parameters:
+ * Draws a circle using the mid-point circle algorithm. Uses only
+ * integer arithmetic.
+ * @brief draws circle
+ *
  * 	@param x0 origin/center of circle
  * 	@param y0 origin/center of circle
  *  @param radius radius of circle
  */
 void GraphicsContext::drawCircle(int x0, int y0, unsigned int radius)
 {
-	// This is a naive implementation that draws many line
-	// segments.  Also uses floating point math for poor performance
+	int x,y;
+	int dx,dy;
+	//decision parameter
+	int D;
 
-	// also, large circles will be "jagged"
-	
-	// start at 0 degrees
-	int oldx = radius;
-	int oldy = 0;
+	x = radius-1;
+	y = 0;
+	dy = 1;
+	dx = 1;
 
-	// go from 1 to 360 degrees
-	for (int segment =1; segment<=360; segment += 1)
-	{
-		int newx = std::cos(segment*M_PI/180)*radius;
-		int newy = std::sin(segment*M_PI/180)*radius;
+	//dx - diameter
+	D	= dx - (radius << 1);
 
-		// hit four quadrants
-		drawLine(x0+oldx,y0+oldy,x0+newx,y0+newy);
-		
-		oldx = newx;
-		oldy = newy;
-		
+	while (x >= y){
+        //draw lower right of circle
+		setPixel(x0 + x, y0 + y);
+		setPixel(x0 + y, y0 + x);
+        //draw lower left of circle
+        setPixel(x0 - x, y0 + y);
+        setPixel(x0 - y, y0 + x);
+		//draw upper right of circle
+        setPixel(x0 + x, y0 - y);
+        setPixel(x0 + y, y0 - x);
+        //draw upper left of circle
+        setPixel(x0 - x, y0 - y);
+        setPixel(x0 - y, y0 - x);
+
+		if (D <= 0 ){
+			// mid-point is on or inside the perimeter
+			y++;
+			D += dy;
+			dy += 2;
+		}else{
+			// mid-point is outside the perimeter
+			x--;
+			dx += 2;
+			//dx - diameter
+			D += dx - (radius << 1);
+		}
 	}
-	
-	return;	
 }
+
 
 void GraphicsContext::endLoop()
 {
--- main.cpp
+++ main.cpp
@@ -5,32 +5,71 @@
  * @project lab 3
  *
  * Discription:
+ * tests the GraphicsContext class
  *
  */
 #include "x11context.h"
 #include <unistd.h>
 #include <iostream>
+#include <cmath>
  
 int main(void)
 {
 	GraphicsContext* gc = new X11Context(800,600,GraphicsContext::BLACK);
- 
- 
-	// draw some lines
+
 	gc->setColor(GraphicsContext::GREEN);
-	gc->setPixel(10,10);
-	gc->setPixel(30,30);
-	gc->drawLine(100,100,100,500);
-	gc->setColor(GraphicsContext::RED);
-	gc->drawLine(100,500,500,500);
-	gc->setColor(GraphicsContext::BLUE);
-	gc->drawLine(500,500,500,100);
-	gc->setColor(GraphicsContext::YELLOW);
-	gc->drawLine(500,100,100,100);
-	gc->setColor(GraphicsContext::MAGENTA);
-	gc->drawCircle(300,300,200);
-	sleep(5);
- 
+    // draw some lines bow & arrow target
+    for (int i = 0; i < 5; ++i) {
+        gc->setColor(GraphicsContext::WHITE);
+
+        gc->drawCircle(300, 300, (200 / 5) * i);
+        if (i == 0) {
+            gc->setColor(GraphicsContext::YELLOW);
+            for (int j = 0; j < (200 / 5); ++j) {
+                gc->drawCircle(300, 300, j);
+            }
+        } else if (i == 1) {
+            gc->setColor(GraphicsContext::RED);
+
+            for (int j = 0; j < (200 / 5); ++j) {
+                gc->drawCircle(300, 300, ((200 / 5)*i)+j);
+            }
+        } else if (i == 2) {
+            gc->setColor(GraphicsContext::CYAN);
+            for (int j = 0; j < (200 / 5); ++j) {
+                gc->drawCircle(300, 300, ((200 / 5)*i)+j);
+            }
+        } else if (i == 4) {
+            gc->setColor(GraphicsContext::WHITE);
+            for (int j = 0; j < (200/5) ; ++j) {
+                gc->drawCircle(300, 300, ((200 / 5)*i)+j);
+            }
+        }
+    }
+    gc->setColor(GraphicsContext::BLUE);
+	sleep(3);
+    gc->clear();
+    // draws sold square using lines
+        for (int k = 0; k <= 180; k += 1) {
+            gc->setColor(GraphicsContext::BLUE-k);
+            gc->drawLine(300, 300, 480, 300 + k);
+            gc->setColor(GraphicsContext::GREEN-(k<<8));
+            gc->drawLine(300, 300, 300 + k, 480);
+            gc->setColor(GraphicsContext::RED-(k<<16));
+            gc->drawLine(300, 300, 480, 300 - k);
+            gc->setColor(GraphicsContext::YELLOW-k);
+            gc->drawLine(300, 300, 300 - k, 480);
+            gc->setColor(GraphicsContext::MAGENTA);
+            gc->drawLine(300, 300, 120, 300 + k);
+            gc->setColor(GraphicsContext::CYAN);
+            gc->drawLine(300, 300, 300 + k, 120);
+            gc->setColor(GraphicsContext::GRAY);
+            gc->drawLine(300, 300, 120, 300 - k);
+            gc->setColor(GraphicsContext::WHITE);
+            gc->drawLine(300, 300, 300 - k, 120);
+
+        }
+    sleep(5);
 	delete gc;
  
 	return 0;
